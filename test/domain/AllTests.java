bd13f3e379f4d2938d8bb904bed534283efd725a
AmineMoussaif <r0855381@ucll.be>
2021-03-29 15:34:26 +0200

    cirkel

--- test/domain/AllTests.java
+++ test/domain/AllTests.java
@@ -1,8 +1,5 @@
 package gui;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ PuntTest.class, SpelerTest.class})
