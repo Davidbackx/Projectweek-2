649eb3b10c70a3a8db7204f1691f2594567ff5f5
Bo <r0838377@ucll.be>
2021-03-29 14:14:47 +0200

    Bo - Lien story 5

---
+++ src/domain/DomainException.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class DomainException extends RuntimeException{
+    public DomainException(){
+        super();
+    }
+
+    public DomainException(String boodschap){
+        super(boodschap);
+    }
+}
