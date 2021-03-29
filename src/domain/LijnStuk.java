04652f360b64980a8dc7e2de474ede68424e01a8
Bo <r0838377@ucll.be>
2021-03-29 14:10:58 +0200

    LijnStuk

--- src/domain/LijnStuk.java
+++ src/domain/LijnStuk.java
@@ -1,4 +1,48 @@
 package domain;
 
 public class LijnStuk {
+    private Punt startPunt;
+    private Punt eindPunt;
+
+    public LijnStuk(Punt startPunt, Punt eindPunt) {
+        if (startPunt == null) {
+            throw new DomainException();
+        }
+        this.startPunt = startPunt;
+        if (eindPunt == null) {
+            throw new DomainException();
+        }
+        this.eindPunt = eindPunt;
+    }
+
+    public Punt getStartPunt() {
+        return startPunt;
+    }
+
+    public Punt getEindPunt() {
+        return eindPunt;
+    }
+
+    public void setStartPunt(Punt startPunt) {
+        this.startPunt = startPunt;
+    }
+
+    public void setEindPunt(Punt eindPunt) {
+        this.eindPunt = eindPunt;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof LijnStuk) {
+            LijnStuk l = (LijnStuk)o;
+            if(this.getStartPunt().equals(l.getStartPunt()) && this.getEindPunt().equals(l.getEindPunt())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "Lijn: startpunt: (" + getStartPunt() + " - eindpunt: " + getEindPunt();
+    }
 }
